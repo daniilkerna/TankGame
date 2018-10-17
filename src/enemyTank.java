2b3f01a7644bfa4c7f4ab767eece95c5317d34f9
Daniil Kerna <danilkak101@gmail.com>
2018-10-17 14:17:19 -0700

    Add enemy tanks

---
+++ src/enemyTank.java
@@ -0,0 +1,63 @@
+import jig.Entity;
+import jig.*;
+
+public class enemyTank extends Entity {
+    public int numberOfBullets = 0;
+
+    private int lives;
+    private float velocity = 2.5f;
+    private int directionFacing = 0;
+
+    public enemyTank (final float x, final float y){
+        super(x , y);
+        this.lives = 1;
+
+        addImageWithBoundingBox(ResourceManager.getImage(TankGame.Enemy_Tank_Up_RSC));
+
+
+    }
+
+
+    public void moveTankLeft(){
+        if (!checkDirectionFaced(1)) {
+            addImage(ResourceManager.getImage(TankGame.Enemy_Tank_Left_RSC));
+        }
+        super.setX(getX() - velocity);
+    }
+
+    public void moveTankRight(){
+        if (!checkDirectionFaced(3)) {
+            addImage(ResourceManager.getImage(TankGame.Enemy_Tank_Right_RSC));
+        }
+        super.setX(getX() + velocity);
+    }
+    public void moveTankUp(){
+        if (!checkDirectionFaced(0)) {
+            addImage(ResourceManager.getImage(TankGame.Enemy_Tank_Up_RSC));
+        }
+
+        super.setY(getY() - velocity);
+    }
+
+    public void moveTankDown(){
+        if (!checkDirectionFaced(2)) {
+            addImage(ResourceManager.getImage(TankGame.Enemy_Tank_Down_RSC));
+        }
+        super.setY(getY() + velocity);
+    }
+
+    private boolean checkDirectionFaced(int facing){
+        if (facing == directionFacing){
+            return true;
+        }
+        else{
+            this.removeAllImages();
+            directionFacing = facing;
+            return false;
+        }
+    }
+
+    public int getDirectionFacing(){
+        return this.directionFacing;
+    }
+}
